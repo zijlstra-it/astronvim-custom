a98a65a2a363d7f7852ad440726c5f4bff188a8c
Sander Zijlstra <sander.zijlstra@surfsara.nl>
2024-10-31 19:49:33 +0100

    Enable tab as buffer switcher

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,8 +1,10 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Tab to switch buffers
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true })
 
 -- Set up custom filetypes
 vim.filetype.add {
